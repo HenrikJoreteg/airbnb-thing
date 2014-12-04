7d2c33d49f5883eba56e0027479af54ce6b59ef5
Henrik Joreteg <hjoreteg@gmail.com>
2014-12-04 13:36:09 -0800

    adding page switcher

--- client/views/main.js
+++ client/views/main.js
@@ -1,8 +1,19 @@
 var View = require('ampersand-view');
 var templates = require('../templates');
+var ViewSwitcher = require('ampersand-view-switcher');
 
 
 module.exports = View.extend({
     template: templates.body,
-    autoRender: true
+    autoRender: true,
+    initialize: function () {
+        this.listenTo(app.router, 'page', this.handlePage);
+    },
+    render: function () {
+        this.renderWithTemplate(this);
+        this.pageSwitcher = new ViewSwitcher(this.queryByHook('page-container'));
+    },
+    handlePage: function (page) {
+        this.pageSwitcher.set(page);
+    }
 });
