61ce4bf4fe146cd90ba457908e24f8a067d046b8
Henrik Joreteg <hjoreteg@gmail.com>
2014-12-04 11:59:23 -0800

    adding router

--- client/app.js
+++ client/app.js
@@ -1,13 +1,18 @@
 var MainView = require('./views/main');
 var domready = require('domready');
+var Router = require('./router');
 
 
 window.app = {
     init: function () {
+        this.router = new Router();
+
         domready(function () {
             this.view = new MainView({
                 el: document.body
             });
+
+            this.router.history.start({pushState: true});
         }.bind(this));
     }
 };
